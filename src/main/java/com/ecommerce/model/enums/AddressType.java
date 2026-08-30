1dd832ec0f0d4da5585703647837853cf96eff68
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-08-30 09:16:24 -0300

    Add AddressType and BannerPosition enums for address and banner management

---
+++ src/main/java/com/ecommerce/model/enums/AddressType.java
@@ -0,0 +1,9 @@
+package com.ecommerce.model.enums;
+
+public enum AddressType {
+    BILLING,
+    SHIPPING,
+    HOME,
+    WORK,
+    OTHER
+}
---
+++ src/main/java/com/ecommerce/model/enums/BannerPosition.java
@@ -0,0 +1,13 @@
+package com.ecommerce.model.enums;
+
+public enum BannerPosition {
+    HOME_HERO,
+    HOME_SIDEBAR,
+    CATEGORY_TOP,
+    PRODUCT_TOP,
+    HEADER,
+    FOOTER,
+    POPUP,
+    SIDEBAR_LEFT,
+    SIDEBAR_RIGHT
+}
