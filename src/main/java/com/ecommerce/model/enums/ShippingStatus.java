9375c3e7c379b9ecf30615a3f8a6cac59799d24c
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-08-30 09:17:55 -0300

    Add enums for ShippingStatus, SortDirection, and SupplierStatus for enhanced shipping and supplier management

---
+++ src/main/java/com/ecommerce/model/enums/ShippingStatus.java
@@ -0,0 +1,13 @@
+package com.ecommerce.model.enums;
+
+public enum ShippingStatus {
+    PENDING,
+    LABEL_CREATED,
+    PICKED_UP,
+    IN_TRANSIT,
+    OUT_FOR_DELIVERY,
+    DELIVERED,
+    DELIVERY_FAILED,
+    RETURNED_TO_SENDER,
+    LOST
+}
